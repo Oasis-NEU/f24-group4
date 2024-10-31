91a2d19887dd721dde6fb5da964ff05a6843853e
Olivia Yeung <oyeung1@gmail.com>
2024-10-31 14:57:41 -0400

    more struggle

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const SPOTIFY_CLIENT_ID = '9699efd9706b432db830fa403b2feca0'
-const REDIRECT_URI='http://127.0.0.1:5500/index.html'
+const REDIRECT_URI='http://localhost:5173/'
 const SCOPES='user-read-private playlist-read-private'
