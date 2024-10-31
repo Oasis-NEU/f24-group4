7c7ca5ff7f222361ae20ae82e37bcf731bbab640
Olivia Yeung <oyeung1@gmail.com>
2024-10-31 15:04:46 -0400

    so much tsrugle

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
 const SPOTIFY_CLIENT_ID = '9699efd9706b432db830fa403b2feca0'
 const REDIRECT_URI='http://localhost:5173/'
 const SCOPES='user-read-private playlist-read-private'
+
+export {SPOTIFY_CLIENT_ID, REDIRECT_URI, SCOPES};
